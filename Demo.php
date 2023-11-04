1b6870973546ceceeba64c9be64dcdb4ed3481af
Ahmed AlKhatib <ahmad.alkhatib@revton.com>
2023-11-04 13:11:28 +0300

    initial commit

---
+++ Demo.php
@@ -0,0 +1,17 @@
+<?php
+
+class Car {
+
+    public $modelNumber;
+    public $manifactureDate;
+
+
+    public function  setModelNumber(){
+
+    }
+
+    public function setManifactureDate(){
+
+    }
+
+}
